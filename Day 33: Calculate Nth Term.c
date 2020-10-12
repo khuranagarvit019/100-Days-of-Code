68853f4c36942e8a4e80ffcc1bae20384756e4f5
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-10-12 22:31:19 +0530

    Day 33: Calculate Nth Term -- HackerRank

---
+++ Day 33: Calculate Nth Term.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+int find_nth_term(int n, int a, int b, int c) {
+  //Write your code here.
+  if(n<0) {
+     n =  (n-a)+(n-b)+(n-c);
+  }
+return n;
+}
+
+
+int main() {
+    int n, a, b, c;
+  
+    scanf("%d %d %d %d", &n, &a, &b, &c);
+    int ans = find_nth_term(n, a, b, c);
+ 
+    printf("%d", ans); 
+    return 0;
+}
+
