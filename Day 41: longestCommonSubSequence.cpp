ace24a757e63ea137a58dadf0403a1037f80b1fc
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-10-21 12:36:52 +0530

    Day 41: longestCommonSubSequence.cpp
    This problem has been solved using the Dynamic Programming Approach.

--- Day 41: longestCommonSubSequence.cpp
+++ Day 41: longestCommonSubSequence.cpp
@@ -6,16 +6,16 @@ int max(int a, int b) {
     return (a>b)? a : b;
 }
 
-int longestCommonSubArray(char *A, char *B, int i, int j) {
+int longestCommonSubSeq(char *A, char *B, int i, int j) {
 
     if (i == 0 || j == 0) {
         return 0;
     }
     if (A[i-1] == B[j-1]) {
-        return 1 + longestCommonSubArray(A,B,i-1,j-1);
+        return 1 + longestCommonSubSeq(A,B,i-1,j-1);
     }
     else {
-        return max(longestCommonSubArray(A,B,i,j-1), longestCommonSubArray(A,B,i-1,j));
+        return max(longestCommonSubSeq(A,B,i,j-1), longestCommonSubSeq(A,B,i-1,j));
     }
 }
 
@@ -26,7 +26,7 @@ int main() {
     int i = strlen(A);
     int j = strlen(B);
 
-    cout<<longestCommonSubArray(A,B,i,j);
+    cout<<longestCommonSubSeq(A,B,i,j);
 
     return 0;
 }
