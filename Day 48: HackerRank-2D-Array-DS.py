661a358fa651441a7481098ad5f0fd25a89ec4dd
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-10-28 21:50:51 +0530

    Create Day 48: HackerRank-2D-Array-DS.py

---
+++ Day 48: HackerRank-2D-Array-DS.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    sum = []
+
+
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            sum.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+        
+    print(max(sum))
+if __name__ == '__main__':
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
