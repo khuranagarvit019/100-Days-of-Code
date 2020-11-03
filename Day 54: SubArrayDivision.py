ea6a65e594cf16f8937da7775fb1ad1311a00c3e
Garvit Khurana <55339445+khuranagarvit019@users.noreply.github.com>
2020-11-03 22:41:02 +0530

    Create Day 54: SubArrayDivision.py

---
+++ Day 54: SubArrayDivision.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthday function below.
+def birthday(s, d, m):
+    tp = (len(s)-m) + 1 #total number of pieces possible
+    return len([1 for i in range(tp) if sum(s[i:i+m])==d])
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    dm = input().rstrip().split()
+
+    d = int(dm[0])
+
+    m = int(dm[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
